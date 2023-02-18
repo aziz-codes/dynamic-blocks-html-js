d953e19bb77be673678bfbe1d7f7815bdeb9b2ec
aziz-codes <azizisoffline@gmail.com>
2023-02-18 18:17:41 +0500

    page redirecting using ajax

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const items = ["Home", "About us", "Support", "Products"];
-
+const container = document.getElementById("container");
 const menus = document.getElementById("menus");
 let lists = "";
 items.forEach((el, index) => {
@@ -9,5 +9,22 @@ items.forEach((el, index) => {
 menus.innerHTML = lists;
 
 function navigate(link) {
-  alert(items[link].replace(" ", "").toLowerCase());
+  //   alert(items[link].replace(" ", "").toLowerCase());
+  let xhr = new XMLHttpRequest();
+  xhr.open(
+    "GET",
+    "./" + items[link].replace(" ", "").toLowerCase() + ".html",
+    true
+  );
+  xhr.onload = () => {
+    if (xhr.readyState === XMLHttpRequest.DONE) {
+      if (xhr.status === 200) {
+        // console.log(xhr.response);
+        container.innerHTML = xhr.response;
+      } else {
+        console.log("something went wrong");
+      }
+    }
+  };
+  xhr.send();
 }
